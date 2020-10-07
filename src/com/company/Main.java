fccae6a02295c777d5d185813b69d34805a5c7b8
paul <paul240702@gmail.com>
2020-10-07 11:35:21 +0200

    Fontion Print Finie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
-
 public class Main {
 
+    /**
+     * Affiche la grille placer en paramètre.
+     * @param grille
+     */
+    public static void PrintGrille(int grille[][]){
+        int colonn=0;
+        int ligne=0;
+        while (ligne<9 && colonn<9)
+            if (ligne==8){
+                System.out.println(grille[ligne][colonn]);
+                ligne=0;
+                colonn=colonn+1;
+            }else {
+                System.out.print(grille[ligne][colonn]);
+                System.out.print(" ");
+                ligne++;
+            }
+
+
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+	PrintGrille(Boards.boardEasy);
+
+
+
+
     }
 }
