6c59b2669313deb687de536576e361b64413d6cc
paul <paul240702@gmail.com>
2020-10-07 13:46:19 +0200

    Fonction imparfaite

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,23 +21,30 @@ public class Main {
         }
 
     }
-    public static boolean GoodLine(int grille[][]){
-        int colonne=0;
+    public static boolean GoodLine(int grille[][],int colonne){
+
         int ligne=0;
-        while (ligne<9 && colonne<9){
-            if (ligne==8){
-                ligne=0;
-                colonne=colonne+1;
-            }else {
+        int [] valeur= new int[9];
+        while (ligne<9 ){
+
+                for (int i=0; i<=valeur.length-1;i++){
+                    if (grille[ligne][colonne]==0 ||grille[ligne][colonne]==valeur[i]){
+                        return false;
+
+                    }
+                }
+                valeur[ligne]= grille[ligne][colonne];
                 ligne++;
             }
-        }
+
         return true;
     }
 
     public static void main(String[] args) {
 
 	PrintGrille(Boards.boardEasy);
+	boolean GoodLine=GoodLine(Boards.boardEasy,1);
+	System.out.print(GoodLine);
 
 
 
