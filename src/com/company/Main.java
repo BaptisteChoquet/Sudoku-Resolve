5014ff77adfd74454c211b551b4e28bd4b3d5edd
Baptiste Choquet <baptiste.choquet@pm.me>
2020-10-08 13:42:45 +0200

    Commit Story 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -152,20 +152,71 @@ public class Main {
         return true ;
     }
 
+    public static void Resolve(int grille [][], int Results[]) {
+        int line=0;
+        int colonn=0;
+        int x = 0 ;
+            while (colonn < 9 && line < 9) {
+                if (x < 81) {
+                    if (colonn == 8) {
+                        colonn = 0;
+                        line = line + 1;
+                        if (grille[colonn][line] != 0) {
+                            x++;
+                        }
+                        else {
+                            colonn = 0 ;
+                            int R[] = Results ;
+                                for (int r = 0 ; r < R.length ; r++) {
+                                    grille[colonn][line] = R[r] ;
+                                    if (!GoodBoard(grille)) {
+                                    }
+                                    else {
+                                        break ;
+                                    }
+                            }
+                        }
+                    }else {
+                        colonn++;
+                        int R[] = Results ;
+                        for (int r = 0 ; r < R.length ; r++) {
+                            grille[colonn][line] = R[r] ;
+                            if (!GoodBoard(grille)) {
+                                if (grille[colonn][line]==8) {
+                                    grille[colonn][line] = 0 ;
+                                }
+                            }
+                            else {
+                                PrintGrille(grille);
+                                System.out.println("______________________");
+                                break ;
+                            }
+                        }
+                    }
+                }
+                else {
+                    PrintGrille(grille);
+                    return ;
+                }
+            }
+    }
+
 
 
 
     public static void main(String[] args) {
 
-	PrintGrille(Boards.boardEasy);
-	boolean GoodLine=GoodLine(Boards.boardEasy,0);
-	System.out.println("la est ligne est-elle bonne ? :" +GoodLine);
-	boolean GoodColonne=GoodColonne(Boards.boardEasy,0);
-	System.out.println("la est colonne est-elle bonne ? :" +GoodColonne);
-	boolean GoodSection = GoodSection(Boards.boardEasy, 3, 3);
-	System.out.println("la sous-section est-elle bonne ? :" + GoodSection);
+//	PrintGrille(Boards.boardEasy);
+//	boolean GoodLine=GoodLine(Boards.boardEasy,0);
+//	System.out.println("la est ligne est-elle bonne ? :" +GoodLine);
+//	boolean GoodColonne=GoodColonne(Boards.boardEasy,0);
+//	System.out.println("la est colonne est-elle bonne ? :" +GoodColonne);
+//	boolean GoodSection = GoodSection(Boards.boardEasy, 3, 3);
+//	System.out.println("la sous-section est-elle bonne ? :" + GoodSection);
     boolean GoodBoards = GoodBoard(Boards.boardEasy);
-    System.out.println("La grille est-elle bonne ? :" +GoodBoards);
+    //    System.out.println("La grille est-elle bonne ? :" +GoodBoards);
+    int Possible[] = {1, 2, 3, 4, 5, 6, 7, 8, 9} ;
+    Resolve(Boards.boardEasy, Possible) ;
 
 
 
