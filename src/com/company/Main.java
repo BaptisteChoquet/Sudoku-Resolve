58b7f30d5ad8a00958e39147ce69b2a45700afe9
leo <leonard.duhem@gmail.com>
2020-10-09 10:47:29 +0200

    Validation du code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,51 +3,51 @@ package com.company;
 public class Main {
 
     /**
-     * Affiche la grille placer en paramètre.
-     * @param grille
+     * Displays the board you choose
+     * @param board Your sudoku board
      */
-    public static void PrintGrille(int grille[][]){
+    public static void PrintGrille(int board[][]){
         int line=0;
-        int colonne=0;
+        int column=0;
 
-        while (colonne<9 && line<9){
-            if (colonne==8){
-                System.out.println(grille[line][colonne]);
-                colonne=0;
+        while (column<9 && line<9){
+            if (column==8){
+                System.out.println(board[line][column]);
+                column=0;
                 line=line+1;
             }else {
-                System.out.print(grille[line][colonne]);
+                System.out.print(board[line][column]);
                 System.out.print(" ");
-                colonne++;
+                column++;
             }
         }
 
     }
 
     /**
-     * Vérifie si une ligne suis la règle du Sudoku.
-     * @param grille
-     * @param i
-     * @return True si la ligne est correct sinon revoie False
+     * Verify if the line is correct
+     * @param board Your sudoku board
+     * @param i the index of the column you want to check
+     * @return True if the line is correct
      */
 
-    public static boolean GoodLine(int grille[][],int i){
+    public static boolean GoodLine(int board[][],int i){
 
         int line=0;
         int [] valeurLine= new int[9];
         while (line<9 ){
             for (int m=0; m<=8;m++) {
-                if (grille[i][line] == 0) {
+                if (board[i][line] == 0) {
 
                 }
                 else {
                     for (int n = 0; n <= 8; n++) {
-                        if (grille[i][line] == valeurLine[n]) {
+                        if (board[i][line] == valeurLine[n]) {
                             return false;
                         }
                     }
                 }
-                valeurLine[line]= grille[i][line];
+                valeurLine[line]= board[i][line];
                 line++;
             }
         }
@@ -55,53 +55,59 @@ public class Main {
     }
 
     /**
-     *
-     * Vérifie si une colonne suis la règle du Sudoku.
-     * @param grille
-     * @param j
-     * @return True si la colonne est correct sinon revoie False
+     * Verify if the column is correct
+     * @param board your sudoku board
+     * @param j index of the line
+     * @return True if the column is true
      */
-    public static boolean GoodColonne(int grille[][],int j){
+    public static boolean GoodColumn(int board[][],int j){
 
-        int colonne=0;
-        int [] valeurColonne= new int[9];
-        while (colonne < 9){
+        int column=0;
+        int [] valeurColumn= new int[9];
+        while (column < 9){
             for (int m=0 ; m<=8 ; m++) {
-                if (grille[colonne][j] == 0) {
+                if (board[column][j] == 0) {
 
                 }
                 else {
                     for (int n = 0; n <= 8; n++) {
-                        if (grille[colonne][j] == valeurColonne[n]) {
+                        if (board[column][j] == valeurColumn[n]) {
                             return false;
                         }
                     }
                 }
-                valeurColonne[colonne]= grille[colonne][j];
-                colonne++;
+                valeurColumn[column]= board[column][j];
+                column++;
             }
         }
         return true;
     }
 
-    public static boolean GoodSection(int grille[][], int i, int j ) {
+    /**
+     * Verify if the section is correct
+     * @param board your sudoku board
+     * @param i the column
+     * @param j the line
+     * @return True when the a square is good
+     */
+    public static boolean GoodSection(int board[][], int i, int j ) {
         int [] valeurSection= new int[9];
         int I = i;
         int J = j;
         while (i < I + 3){
             while (j < J + 3) {
                 for (int m = 0; m < 3; m++) {
-                    if (grille[i][j] == 0) {
+                    if (board[i][j] == 0) {
 
                     } else {
                         for (int n = 0; n < 9; n++) {
-                            if (grille[i][j] == valeurSection[n]) {
+                            if (board[i][j] == valeurSection[n]) {
                                 return false;
                             }
                         }
                     }
                     int index = (i-I)*3 + j-J;
-                    valeurSection[index] = grille[i][j];
+                    valeurSection[index] = board[i][j];
                     j++;
                 }
                 i++;
@@ -112,7 +118,12 @@ public class Main {
         return true ;
     }
 
-    public static boolean GoodBoard (int grille[][]) {
+    /**
+     * Verify if your board is correct
+     * @param board Your sudoku board
+     * @return true if the the board is the correct
+     */
+    public static boolean GoodBoard (int board[][]) {
         int i = 0 ;
         int j = 0 ;
         int I = i ;
@@ -120,16 +131,16 @@ public class Main {
 
         while (i < 9 && j < 9) {
             while (i < 9) {
-                boolean GoodLines = GoodLine(grille, i);
-                GoodLine(grille, i);
+                boolean GoodLines = GoodLine(board, i);
+                GoodLine(board, i);
                 if (!GoodLines) {
                     return false;
                 }
                 i++;
             }
             while (j < 9) {
-                boolean GoodColonnes = GoodColonne(grille, j);
-                GoodColonne(grille, j);
+                boolean GoodColonnes = GoodColumn(board, j);
+                GoodColumn(board, j);
                 if (!GoodColonnes) {
                     return false ;
                 }
@@ -139,8 +150,8 @@ public class Main {
             j = J ;
             while (i < 9) {
                 while (j < 9) {
-                    boolean GoodSections = GoodSection(grille , i , j) ;
-                    GoodSection(grille , i , j ) ;
+                    boolean GoodSections = GoodSection(board , i , j) ;
+                    GoodSection(board , i , j ) ;
                     if (!GoodSections) {
                         return false;
                     }
@@ -153,29 +164,36 @@ public class Main {
         return true ;
     }
 
-    public static boolean Resolve (int grille [][]) {
+    /**
+     * Resolve your sudoku board
+     * @param board Your sudoku board
+     * @return true when the board is resolved
+     */
+    public static boolean Resolve (int board [][]) {
         int i ;
         int j ;
         for (j = 0 ; j < 9 ; j++) {
             for (i = 0 ; i < 9 ; i++){
-                if  (grille[i][j] == 0) {
+                if  (board[i][j] == 0) {
                     for (int possibilité = 1 ; possibilité < 10 ; possibilité++) {
-                        grille[i][j] = possibilité;
-                        if (GoodBoard(grille)) {
-                            if (Resolve(grille)) {
+                        CPU ++ ;
+                        board[i][j] = possibilité;
+                        if (GoodBoard(board)) {
+                            if (Resolve(board)) {
                                 return true;
                             }
                         }
                     }
-                    grille[i][j] = 0 ;
+                    board[i][j] = 0 ;
                     return false ;
                 }
             }
         }
-        PrintGrille(grille);
+        PrintGrille(board);
         System.out.println("_________________");
         return false ;
     }
+    static int CPU ;
 
     public static void main(String[] args) {
 
@@ -186,7 +204,7 @@ public class Main {
     Resolve(Boards.boardGodLike);
     long timerEnd = System.currentTimeMillis() ;
     long timer = timerEnd - timerStart ;
-    System.out.println("Le sudoku c'est résolu en " + timer + "ms");
+    System.out.println("The board was resolved in " + timer + " ms and " + CPU + " tries");
 
     }
 }
